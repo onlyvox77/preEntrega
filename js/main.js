7d0f2e0c55ab1cd5fafff229b078a5e6e0c96d29
onlyvox77 <143464123+onlyvox77@users.noreply.github.com>
2023-11-13 19:49:04 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const dolarBlue = 959;
+const dolarBlue = 960;
+
 
 let saldo = Number(prompt("Cuanto dinero quieres invertir?"));
     console.log("Tienes $" + saldo);
@@ -32,3 +33,19 @@ let resultado = resta( saldo, dolarBlue);
 console.log(resultado)
 console.log("$" + resultado + ". Este es el saldo que te sobro o lo que te falta para poder seguir comprando.")
 
+function tipoDolar(n, v, c) {
+    this.nombre = n
+    this.valor =
+    this.compra = c
+}
+
+const blue = new tipoDolar ("Dolar Blue", "$960", "$940")
+console.log(blue)
+const oficial = new tipoDolar ("Dolar Oficial", "$368", "$349")
+console.log(oficial)
+const solidario = new tipoDolar ("Dolar Solidario", "$640", "$640")
+console.log(solidario)
+const bolsa = new tipoDolar ("Dolar Bolsa", "$903", "$884")
+console.log(bolsa)
+const cripto = new tipoDolar ("Dolar Cripto", "$897", "$888")
+console.log(cripto)
